1b67a6a6e9b9155d170cddabdd8042ccbf12294d
Aidan O'Grady <theaidanogrady@gmail.com>
2015-10-02 14:26:32 +0100

    Created the pom file and Driver class which will handle basic stuff for now. As the project expands, this will naturally be vastly mutated.

---
+++ src/main/java/cs412/group_fifteen/main/Driver.java
@@ -0,0 +1,23 @@
+package cs412.group_fifteen.main;
+
+import org.apache.lucene.demo.IndexFiles;
+import org.apache.lucene.demo.SearchFiles;
+
+/**
+ * Main method class.
+ *
+ * @author Aidan O'Grady
+ * @version 1.0
+ * @since 1.0
+ */
+public class Driver {
+
+    public static void main(String[] args) throws Exception
+    {
+        String[] indexArgs = {"-index", "index", "-docs", "files"};
+        IndexFiles.main(indexArgs);
+        SearchFiles.main(args);
+    }
+
+
+}
