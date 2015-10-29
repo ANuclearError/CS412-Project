51784f29440ce61207ada33df0e21f66cb63af15
Aidan O'Grady <aidan@aidanogrady.com>
2015-10-29 11:31:50 +0000

    STarted making GUI for search system.

---
+++ java-ebook-search/src/main/java/com/aidanogrady/java_ebook_search/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.aidanogrady.java_ebook_search.controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * @author Aidan O'Grady
+ * @since 0.0
+ */
+public class Controller {
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void about(ActionEvent actionEvent) {
+        System.out.println("About");
+    }
+}
