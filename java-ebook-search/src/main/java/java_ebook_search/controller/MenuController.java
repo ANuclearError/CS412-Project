a8cbcd2c7bdb53b6fabad8ac09af3baa5309aa99
Aidan O'Grady <aidan@aidanogrady.com>
2015-11-20 11:24:42 +0000

    Changed file structure and also divded GUI into separate files for ease of use.

---
+++ java-ebook-search/src/main/java/java_ebook_search/controller/MenuController.java
@@ -0,0 +1,27 @@
+package java_ebook_search.controller;
+
+/**
+ * The MenuController class handles the GUI events, calling the methods that will
+ * interact with Lucene.
+ *
+ * @author Aidan O'Grady
+ * @since 0.0
+ */
+public class MenuController {
+
+    /**
+     * Displays about info.
+     *
+     * TODO: Create about window.
+     */
+    public void about() {
+        System.out.println("About");
+    }
+
+    /**
+     * Quits the system.
+     */
+    public void exit() {
+        System.exit(0);
+    }
+}
---
+++ java-ebook-search/src/main/java/java_ebook_search/model/SpellCheck.java
@@ -0,0 +1,65 @@
+package java_ebook_search.model;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.BritishEnglish;
+import org.languagetool.rules.RuleMatch;
+import org.languagetool.rules.UppercaseSentenceStartRule;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The SpellCheck class is responsible for handling the dynamic spell check and
+ * suggestions for the system.
+ *
+ * @author Aidan O'Grady
+ * @since 0.1
+ */
+public class SpellCheck {
+
+    /**
+     * LanguageTool that handles the spell checking.
+     */
+    private JLanguageTool tool;
+
+    /**
+     * Constructor
+     */
+    public SpellCheck() {
+        tool = new JLanguageTool(new BritishEnglish());
+
+    }
+
+    /**
+     * Given a search term, returns a list of potential suggestions for the
+     * user.
+     * @param term
+     *  The search query entered.
+     * @return
+     *  Suggested terms.
+     * @throws IOException
+     *
+     * TODO: Implement a better system.
+     */
+    public List<String> getSuggestions(String term) throws IOException {
+        List<String> list = new ArrayList<String>();
+
+        List<RuleMatch> matches = tool.check(term);
+
+        for (RuleMatch match : matches) {
+
+            if(!(match.getRule() instanceof UppercaseSentenceStartRule)) {
+                for(String suggested : match.getSuggestedReplacements()) {
+                    String string = "";
+                    if (match.getFromPos() > 0)
+                        string += term.substring(0, match.getFromPos());
+                    string += suggested;
+                    string += term.substring(match.getToPos(), term.length());
+                    list.add(string);
+                }
+            }
+        }
+        return list;
+    }
+}
