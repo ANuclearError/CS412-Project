b40f8a46dd47a974b7d3da72babe1dc3288b8d1d
Aidan O'Grady <aidan@aidanogrady.com>
2015-12-14 14:18:01 +0000

    Cleaned Book

--- java-ebook-search/src/main/java/java_ebook_search/model/Book.java
+++ java-ebook-search/src/main/java/java_ebook_search/model/Book.java
@@ -2,13 +2,26 @@ package java_ebook_search.model;
 
 /**
  * Enum for storing book titles.
+ *
+ * @author Aidan O'Grady
+ * @since 0.4?
  */
 public enum Book {
-	JAVANUT("Java in a Nutshell"), LANGREF("Java Language Reference"), AWT("Java AWT Reference"), FCLASS(
-			"Java Fundamental Classes Reference"), EXP("Exploring Java");
+	JAVANUT("Java in a Nutshell"),
+	LANGREF("Java Language Reference"),
+	AWT("Java AWT Reference"),
+	FCLASS("Java Fundamental Classes Reference"),
+	EXP("Exploring Java");
 
+	/**
+	 * The title of the book, used for enum's toString.
+	 */
 	private String title;
 
+	/**
+	 * Constructor for the enums
+	 * @param title - the title of the book.
+	 */
 	Book(String title) {
 		this.title = title;
 	}
