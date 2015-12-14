b1a815f3f14b01480a21092fb0765c177a0be542
Aidan O'Grady <aidan@aidanogrady.com>
2015-12-14 13:01:54 +0000

    Removed unused imports.

--- java-ebook-search/src/main/java/java_ebook_search/controller/FiltersController.java
+++ java-ebook-search/src/main/java/java_ebook_search/controller/FiltersController.java
@@ -1,10 +1,7 @@
 package java_ebook_search.controller;
 
-import java.net.URL;
 import java.util.HashSet;
-import java.util.ResourceBundle;
 import java.util.Set;
-
 import java_ebook_search.model.Book;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
